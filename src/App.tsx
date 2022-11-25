7885490ca4cbccbdfe190f760a554e943f84aafa
NikolayZhulin <nikolayzhulin97@mail.ru>
2022-11-25 17:18:02 +0100

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type PropsType = {
+	body: string
+	id: number
+	title: string
+	userId: number
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [state, setState] = useState<Array<PropsType>>([])
+	
+	const deleteHandler = () => {
+		setState([])
+	}
+	
+	const showPostsHandler = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => setState(json))
+	}
+	
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/posts')
+			.then(response => response.json())
+			.then(json => setState(json))
+	}, [])
+	return (
+		<div className="App">
+			<button onClick={deleteHandler}>DELETE</button>
+			<button onClick={showPostsHandler}>Show</button>
+			<ul>
+				{state.map(elem => {
+					return (
+						<li key={elem.id}>
+							<span>{elem.title}</span>
+						</li>
+					)
+				})}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
